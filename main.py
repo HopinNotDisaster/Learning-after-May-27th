efda0bba4034cab90f41b55dc3327b1d8b2983c8
HopinNotDisaster <syq3315813594@gmail.com>
2024-05-27 10:56:51 +0800

    初始化本地！

---
+++ main.py
@@ -0,0 +1,6 @@
+print("hello word!")
+
+url = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API key}"
+
+# Replace '{city}' and '{API key}' with your actual values
+city = "London"
